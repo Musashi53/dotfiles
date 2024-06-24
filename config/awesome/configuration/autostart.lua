457dc327feebf31c952634545dd7a56ebdc76d0d
Musashi53 <coder73854@proton.me>
2024-06-24 16:45:25 -0300

    :fire: remove bspwm and awesome rices

--- config/awesome/configuration/autostart.lua
+++
@@ -1,6 +0,0 @@
-require("configuration.init")
-
-awful.spawn("lxsession")
-awful.spawn("picom --animations --backend glx --animation-for-open-window fly-in -b")
-
-awful.spawn("xscreensaver --no-splash")
